7c913592636d67ec9f315c79bf266f359152d098
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-30 19:02:47 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,32 @@
 def consolidate_cart(cart)
-  # code here
+  new_hash = Hash.new{|hsh,key| hsh[key] = {:count => 0}}
+  
+  cart.collect do |hash_chunk|
+    hash_chunk.collect do |vegetable, price_stuff|
+      price_stuff.collect do |k, v|
+        
+      new_hash[vegetable].store(k, v)
+     
+
+    end
+    new_hash[vegetable][:count] += 1
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  cart.each do |vegetable, chunk|
+    puts vegetable({
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 1},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1},
+  "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1},
+}, {:item => "AVOCADO", :num => 2, :cost => 5.0})
+  end
 end
-
+apply_coupons
 def apply_clearance(cart)
-  # code here
+  
 end
 
 def checkout(cart, coupons)
