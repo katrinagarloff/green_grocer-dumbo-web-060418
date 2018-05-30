56338ad98678a0603dacb89b8468a2f3673471f9
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-30 19:03:17 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -16,15 +16,9 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  cart.each do |vegetable, chunk|
-    puts vegetable({
-  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 1},
-  "KALE"    => {:price => 3.0, :clearance => false, :count => 1},
-  "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1},
-}, {:item => "AVOCADO", :num => 2, :cost => 5.0})
-  end
+ 
 end
-apply_coupons
+
 def apply_clearance(cart)
   
 end
