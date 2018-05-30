141a05c8b8380a99c98fce36f68315f261105fdd
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-30 20:52:43 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -58,7 +58,7 @@ apply_coupons({
   "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
 }, 	[
 		{:item => "AVOCADO", :num => 2, :cost => 5.00},
-		{:item => "AVOCADO", :num => 2, :cost => 20.00},
+		{:item => "KALE", :num => 2, :cost => 20.00},
 		{:item => "CHEESE", :num => 3, :cost => 15.00}
 	])
 
